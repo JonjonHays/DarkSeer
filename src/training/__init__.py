51515ab52995a7acee2197b06e7aa1d08bedfdcd
JonjonHays <mightyfancy@gmail.com>
2026-01-12 23:45:41 -0800

    training pipeline first pass

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,6 +1,13 @@
 # DarkSeer Training Module
 
 from .data_collector import CatastropheDataCollector
+from .dataset import DarkSeerDataset, TrainingExample, load_catastrophes, load_safe_commits
 
-__all__ = ["CatastropheDataCollector"]
+__all__ = [
+    "CatastropheDataCollector",
+    "DarkSeerDataset",
+    "TrainingExample",
+    "load_catastrophes",
+    "load_safe_commits",
+]
 
