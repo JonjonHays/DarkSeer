223d85e645f2ca0b74c54bb462b10c956c626a23
JonjonHays <mightyfancy@gmail.com>
2026-01-12 23:26:01 -0800

    Add training data collection

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,7 +1,6 @@
 # DarkSeer Training Module
 
 from .data_collector import CatastropheDataCollector
-from .dataset import DarkSeerDataset
 
-__all__ = ["CatastropheDataCollector", "DarkSeerDataset"]
+__all__ = ["CatastropheDataCollector"]
 
